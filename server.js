02a28617a210db1765d476a620898c4cb2e58cbc
Ben McLoughlin <benmcl@shaw.ca>
2020-12-11 14:14:34 -0800

    set node version

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there from savvyplan 3.2" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
