b13e68037438822a5dcf92f0b0e26cd9a7422a7c
Ben McLoughlin <benmcl@shaw.ca>
2020-12-11 14:17:44 -0800

    added backend files

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signupUser);
+router.post('/login', authController.loginUser);
+router.get('/logout', authController.logout);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.get('/getProfile/:id', userController.getProfileById);
+
+//all below are protected
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+module.exports = router;
